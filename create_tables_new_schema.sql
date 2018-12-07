c627ea37fd03c3ed996d0e0d05f75e34898973d2
Marlo Zeroth <Program0@users.noreply.github.com>
2018-12-06 21:44:55 -0800

    SQL Scripts for postgre
    Scripts to load data into the sentiment_tweets table and to create indexes.

---
+++ create_tables_new_schema.sql
@@ -0,0 +1,222 @@
+------------------
+---Marlo Zeroth---
+----11/26/2018----
+------------------
+
+--------------
+---Clean up---
+--------------
+
+---CREATE TABLE tweets_copy AS
+---SELECT * FROM sentiment_tweet;
+
+DROP TABLE IF EXISTS sentiment_tweet CASCADE;--OK
+--DROP TABLE IF EXISTS tweets CASCADE;--OK
+------------
+---TABLES---
+------------
+
+CREATE TABLE sentiment_tweet (   
+    id SERIAL NOT NULL,
+    geo CHAR (6) NOT NULL,
+    hashtag VARCHAR(280),
+    tweetkey NUMERIC(19,0) NOT NULL,
+    latitude NUMERIC(9,6),
+    longitude NUMERIC(9,6),
+    mention VARCHAR(280),
+    tweet VARCHAR(1024) NOT NULL,
+    time_stamp TIMESTAMPTZ NOT NULL,
+    tweeturl VARCHAR(128),
+    user_name VARCHAR(280) NOT NULL,
+    sentiment NUMERIC(1,0) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+----------------
+--Import Data---
+----------------
+
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181104.csv'
+WITH DELIMITER ',';
+
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181105.csv'
+WITH DELIMITER ',';
+
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181106.csv'
+WITH DELIMITER ',';
+
+        
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181107.csv'
+WITH DELIMITER ',';
+
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181122.csv'
+WITH DELIMITER ',';
+--- Error
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181123.csv'
+WITH DELIMITER ',';
+
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181124.csv'
+WITH DELIMITER ',';
+
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181129.csv'
+WITH DELIMITER ',';
+                                
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181130.csv'
+WITH DELIMITER ',';
+
+                                
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181201.csv'
+WITH DELIMITER ',';
+
+
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181202.csv'
+WITH DELIMITER ',';
+
+
+COPY sentiment_tweet (
+    geo,
+    hashtag,
+    tweetkey,
+    latitude,
+    longitude,
+    mention,
+    tweet,
+    time_stamp,
+    tweeturl,
+    user_name,
+    sentiment)
+FROM 'c:/postgresql/scripts/sentimentTweets20181203.csv'
+WITH DELIMITER ',';
+                                                                                                                                                
---
+++ hashtag_mention_index.sql
@@ -0,0 +1,43 @@
+--- Marlo Zeroth
+--- 861309346
+--- Indeces to optimize most made queries.
+
+CREATE INDEX IF NOT EXISTS hastag_mention_index
+ON sentiment_tweet
+USING BTREE
+(hashtag, mention);
+
+CREATE INDEX IF NOT EXISTS hashtag_lower_index
+ON sentiment_tweet
+USING BTREE
+(lower(hashtag));
+
+CREATE INDEX IF NOT EXISTS metion_lower_index
+ON sentiment_tweet
+USING BTREE
+(lower(mention));
+
+CREATE INDEX  IF NOT EXISTS hashtag_index
+ON sentiment_tweet
+USING BTREE
+(hashtag);
+
+CREATE INDEX  IF NOT EXISTS mention_index
+ON sentiment_tweet
+USING BTREE
+(mention);
+
+CREATE INDEX  IF NOT EXISTS user_index
+ON sentiment_tweet
+USING BTREE
+(user_name);
+
+CREATE INDEX  IF NOT EXISTS time_index
+ON sentiment_tweet
+USING BTREE
+(time_stamp);
+
+CREATE INDEX  IF NOT EXISTS geo_index
+ON sentiment_tweet
+USING BTREE
+(geo);
